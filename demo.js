19fc99fa24e2c872d92d2036f271e5c094dab2a3
Akashkolekar <akashkolekar07@gmail.com>
2022-10-04 17:07:26 +0530

    First program execution in JS
    First program execution in JS

---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("My Name Is Akash Nanaso Kolekar");
+console.log("My Hobbies: 1)Travelling, 2)Football, 3)Gaming");
+console.info("Personal Strength: 1)Hardworking, 2)Creativity, 3)Quick Learner");
+console.warn("Weakness: 1)Impatience 2)Sensitive 3)I Trust people easily");
+console.log(30+50);
+console.log(49*479);
