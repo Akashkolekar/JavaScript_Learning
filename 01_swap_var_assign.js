3f6a251d6298ec09ee3bd8c7f43a4e4f35de4797
Akashkolekar <akashkolekar07@gmail.com>
2022-10-06 12:51:23 +0530

    variable and swap variable
    variable and swap variable

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,21 @@
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("Before Swap:");
+console.log(sweety, cutie);
+
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("After Swap:");
+console.log(sweety, cutie);
+
+var num1 = 100, num2 = 200, num3 = 300;
+console.log("Before Swap:");
+console.log(num1, num2, num3);
+
+var temp = num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After Swap:");
+console.log(num1, num2, num3);
---
+++ 01_variable_assign.js
@@ -0,0 +1,26 @@
+var myFullName;
+console.log(myFullName);
+myFullName = "Akash Kolekar"
+console.log("First Name and Last Name:", myFullName);
+myFullName = "Akash Nanaso Kolekar"
+console.log("Updated Full Name:",myFullName);
+
+var myLove = "I love only JS"
+console.log(myLove);
+
+var firstName = "Akash";
+console.log("First Name:", firstName);
+var lastName = "Kolekar";
+console.log("Last Name:", lastName);
+var age = "24";
+console.log("Age:", age);
+var address = "Hira Colony, Unchgaon, Tal-Karveer, Kolhapur";
+console.log("Address:", address);
+var pin_code = "416005";
+console.log("Pin Code:", pin_code);
+var lastName = "Kolekar";
+console.log("Last Name:", lastName);
+var gender = "Male";
+console.log("Gender:", gender);
+var is_married = "Unmarried";
+console.log("Is Married:", is_married);
